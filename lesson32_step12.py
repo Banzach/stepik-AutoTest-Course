6b70f1b2564947b420e2fa6cb16b8ab63ac972a9
Urchenko.SV <Urchenko.SV@JG>
2019-12-13 15:46:01 +0300

    Добавил файл по задаче #12, основы Unittest

---
+++ lesson32_step12.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import unittest
+import time
+import os
+import pyautogui
+
+link = "https://www.imperiasumok.ru/"
+
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+
+    def test_abs2(self):
+        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+
+if __name__ == "__main__":
+    unittest.main()
+    print("Everything passed")
